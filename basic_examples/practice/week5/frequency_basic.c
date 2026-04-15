bf914d6e09da000cada994b90be98b57826a6db3
HUNG QUANG NGUYEN <nguyenquanghung712007@gmail.com>
2026-04-15 17:57:32 +0700

    update cua Rin

---
+++ basic_examples/practice/week5/frequency_basic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int mark[1000001] = {0};
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++){
+        if(mark[arr[i]] == 0){
+            printf("%d ", arr[i]);
+            mark[arr[i]] = 1;
+        }
+    }
+}
